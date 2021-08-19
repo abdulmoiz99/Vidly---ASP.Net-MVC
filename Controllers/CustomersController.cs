a6cfd58187c19599ad88bbe267d59e672f67d7d2
Abdul Moiz <moiza0799@gmail.com>
2021-08-19 09:55:49 -0700

    Minor Changes

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -22,9 +22,23 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customer = _context.Customers;
+            var customer = _context.Customers.ToList();
 
             return View(customer);
         }
+        public ActionResult Details(int Id)
+        {
+            var customer = _context.Customers.SingleOrDefault(x => x.Id == Id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
     }
 }
