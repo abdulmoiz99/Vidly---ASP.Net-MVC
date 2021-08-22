0ed0117e6bdefc09b9f63cb8c26931c13ad1f12b
Abdul Moiz <moiza0799@gmail.com>
2021-08-22 01:09:47 -0700

    Updating Data

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -37,9 +37,24 @@ namespace Vidly.Controllers
             };
             return View("CustomerForm", ViewModel);
         }
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id ==0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribeToNewsLetter = customer.IsSubscribeToNewsLetter;
+
+
+                //TryUpdateModel(customerInDb); will automatically update all the fields 
+            }
+
             _context.SaveChanges();
             return RedirectToAction("Index", "Customers");
         }
